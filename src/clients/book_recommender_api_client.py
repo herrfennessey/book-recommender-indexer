7b7d5ac32319e1858fbccd54f4025b454def8a4c
Dave Fennessey <dave@dfennessey.com>
2023-03-11 14:15:11 +0100

    get_books_read + testing

--- src/clients/book_recommender_api_client.py
+++ src/clients/book_recommender_api_client.py
@@ -1,7 +1,10 @@
 import logging
 from functools import lru_cache
+from typing import List
 
 import httpx
+from asyncache import cached
+from cachetools import LRUCache, TTLCache
 from fastapi import Depends
 
 from src.clients.api_models import BookDataV1
@@ -27,7 +30,7 @@ class BookRecommenderApiClient(object):
             response = httpx.put(url, data=book.json())
             if not response.is_error:
                 logger.info("Successfully wrote book: {}".format(book_id))
-                return response
+                return
             elif response.is_client_error:
                 logger.error(
                     "Received 4xx exception from server with body: {} URL: {} "
@@ -44,6 +47,28 @@ class BookRecommenderApiClient(object):
             logging.error(
                 "Uncaught Exception: {} encountered for URL: {} for book_id: {}".format(e, url, book_id))
             raise BookRecommenderApiServerException("Uncaught Exception encountered for book id: {}".format(book_id))
+
+    @cached(TTLCache(maxsize=1024, ttl=600))
+    async def get_books_read_by_user(self, user_id) -> List[int]:
+        url = f"{self.base_url}/users/{user_id}/book-ids"
+        try:
+            response = httpx.get(url)
+            if not response.is_error:
+                return response.json().get("book_ids", [])
+            elif response.is_client_error:
+                logger.info("Received 4xx exception from server, assuming user {} does not exist. URL: {} ".format(
+                    user_id, url))
+                return []
+            elif response.is_server_error:
+                logger.error(
+                    "Received 5xx exception from server with body: {} URL: {} user_id: {}".format(response.text, url,
+                                                                                               user_id))
+                raise BookRecommenderApiServerException(
+                    "5xx Exception encountered {} for user_id: {}".format(response.text, user_id))
+        except httpx.HTTPError as e:
+            logging.error(
+                "Uncaught Exception: {} encountered for URL: {} for user_id: {}".format(e, url, user_id))
+            raise BookRecommenderApiServerException("Uncaught Exception encountered for user_id: {}".format(user_id))
 
 
 class BookRecommenderApiClientException(Exception):
--- tests/clients/test_book_recommender_api_client.py
+++ tests/clients/test_book_recommender_api_client.py
@@ -14,24 +14,23 @@ TEST_PROPERTIES = Properties(book_recommender_api_base_url="https://testurl", en
 
 
 @pytest.mark.asyncio
-async def test_successful_response_from_user_info_client(httpx_mock, caplog: LogCaptureFixture):
+async def test_successful_book_put(httpx_mock, caplog: LogCaptureFixture):
     # Given
     caplog.set_level("INFO", logger="book_recommender_api_client")
-    httpx_mock.add_response(json={"goofed": "you did"}, status_code=200, url="https://testurl/books/1")
+    httpx_mock.add_response(json={}, status_code=200, url="https://testurl/books/1")
     client = BookRecommenderApiClient(properties=TEST_PROPERTIES)
 
     # When
-    response = await client.create_book(_a_random_book())
+    await client.create_book(_a_random_book())
 
     # Then
     assert_that(caplog.text).contains("Successfully wrote book: 1")
-    assert_that(response.status_code).is_equal_to(200)
 
 
 @pytest.mark.parametrize("expected_response_code", [500, 501, 502, 503, 504])
 @pytest.mark.asyncio
-async def test_5xx_custom_exception_from_user_info_client(expected_response_code, httpx_mock,
-                                                          caplog: LogCaptureFixture):
+async def test_5xx_custom_exception_on_book_put(expected_response_code, httpx_mock,
+                                                caplog: LogCaptureFixture):
     # Given
     json_response = {"goofed": "you did"}
     caplog.set_level("ERROR", logger="book_recommender_api_client")
@@ -53,8 +52,8 @@ async def test_5xx_custom_exception_from_user_info_client(expected_response_code
 
 @pytest.mark.parametrize("expected_response_code", [400, 401, 402, 403, 404])
 @pytest.mark.asyncio
-async def test_4xx_custom_exception_from_user_info_client(expected_response_code, httpx_mock,
-                                                          caplog: LogCaptureFixture):
+async def test_4xx_custom_exception_on_book_put(expected_response_code, httpx_mock,
+                                                caplog: LogCaptureFixture):
     # Given
     json_response = {"goofed": "you did"}
     caplog.set_level("ERROR", logger="book_recommender_api_client")
@@ -75,7 +74,7 @@ async def test_4xx_custom_exception_from_user_info_client(expected_response_code
 
 
 @pytest.mark.asyncio
-async def test_uncaught_exception_from_user_info_client(httpx_mock, caplog: LogCaptureFixture):
+async def test_uncaught_exception_on_put_book(httpx_mock, caplog: LogCaptureFixture):
     # Given
     caplog.set_level("ERROR", logger="book_recommender_api_client")
     httpx_mock.add_exception(httpx.ReadTimeout("Unable to read within timeout"))
@@ -90,6 +89,66 @@ async def test_uncaught_exception_from_user_info_client(httpx_mock, caplog: LogC
         "https://testurl/books/1",
         "book_id: 1"
     )
+
+
+@pytest.mark.asyncio
+async def test_successful_get_books_read(httpx_mock, caplog: LogCaptureFixture):
+    # Given
+    caplog.set_level("INFO", logger="book_recommender_api_client")
+    httpx_mock.add_response(json={"book_ids": [3, 4, 5]}, status_code=200, url="https://testurl/users/1/book-ids")
+    client = BookRecommenderApiClient(properties=TEST_PROPERTIES)
+
+    # When
+    response = await client.get_books_read_by_user(1)
+
+    # Then
+    assert_that(response).contains_only(3, 4, 5)
+
+
+@pytest.mark.asyncio
+async def test_4xx_when_getting_books_read_returns_empty_array(httpx_mock, caplog: LogCaptureFixture):
+    # Given
+    caplog.set_level("INFO", logger="book_recommender_api_client")
+    httpx_mock.add_response(status_code=404, url="https://testurl/users/1/book-ids")
+    client = BookRecommenderApiClient(properties=TEST_PROPERTIES)
+
+    # When
+    response = await client.get_books_read_by_user(1)
+
+    # Then
+    assert_that(caplog.text).contains("Received 4xx exception from server", "user_id: 1",
+                                      "https://testurl/users/1/book-ids")
+    assert_that(response).is_equal_to(list())
+
+
+@pytest.mark.asyncio
+async def test_5xx_when_getting_books_read_throws_exception(httpx_mock, caplog: LogCaptureFixture):
+    # Given
+    caplog.set_level("INFO", logger="book_recommender_api_client")
+    httpx_mock.add_response(status_code=500, url="https://testurl/users/1/book-ids")
+    client = BookRecommenderApiClient(properties=TEST_PROPERTIES)
+
+    # When / Then
+    with pytest.raises(BookRecommenderApiServerException):
+        await client.get_books_read_by_user(1)
+
+    assert_that(caplog.text).contains("Received 5xx exception from server", "user_id: 1",
+                                      "https://testurl/users/1/book-ids")
+
+
+@pytest.mark.asyncio
+async def test_unhandled_exceptions_when_getting_books_read_throws_exception(httpx_mock, caplog: LogCaptureFixture):
+    # Given
+    caplog.set_level("INFO", logger="book_recommender_api_client")
+    httpx_mock.add_exception(httpx.ReadTimeout("Unable to read within timeout"))
+    client = BookRecommenderApiClient(properties=TEST_PROPERTIES)
+
+    # When / Then
+    with pytest.raises(BookRecommenderApiServerException):
+        await client.get_books_read_by_user(1)
+
+    assert_that(caplog.text).contains("Uncaught Exception", "Unable to read within timeout", "user_id: 1",
+                                      "https://testurl/users/1/book-ids")
 
 
 @pytest.fixture
