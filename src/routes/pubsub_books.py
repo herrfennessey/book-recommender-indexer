ba103cb3567a180e5966596c65505924ec9e56df
Dave Fennessey <dave@dfennessey.com>
2023-03-20 21:07:44 +0100

    book audit with tests

--- src/routes/pubsub_books.py
+++ src/routes/pubsub_books.py
@@ -1,4 +1,5 @@
 import logging
+from functools import lru_cache
 
 from fastapi import APIRouter, Depends
 from pydantic import ValidationError
@@ -7,8 +8,16 @@ from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR
 
 from src.clients.book_recommender_api_client import BookRecommenderApiClient, get_book_recommender_api_client, \
     BookRecommenderApiClientException, BookRecommenderApiServerException
+from src.clients.pubsub_audit_client import get_pubsub_audit_client, PubsubAuditClient
+from src.dependencies import Properties
 from src.routes.pubsub_models import PubSubMessage, IndexerResponse, PubSubBookV1
 from src.routes.pubsub_utils import _unpack_envelope
+
+
+@lru_cache()
+def get_properties():
+    return Properties()
+
 
 logger = logging.getLogger(__name__)
 router = APIRouter(prefix="/pubsub/books")
@@ -33,7 +42,8 @@ The message pubsub sends us roughly follows this schema - data is base 64 encode
 @router.post("/handle", tags=["books"], status_code=200)
 async def handle_pubsub_message(
         request: PubSubMessage,
-        client: BookRecommenderApiClient = Depends(get_book_recommender_api_client)
+        client: BookRecommenderApiClient = Depends(get_book_recommender_api_client),
+        pubsub_audit_client: PubsubAuditClient = Depends(get_pubsub_audit_client)
 ):
     """
     Handle a pubsub POST call. We do not use the actual pubsub library, but instead receive the message
@@ -44,7 +54,7 @@ async def handle_pubsub_message(
     """
     indexed = 0
     batch = _unpack_envelope(request)
-
+    successful_books = []
     # We have a valid book batch, so let's send it to the book recommender API
     try:
         for book in batch.items:
@@ -53,19 +63,18 @@ async def handle_pubsub_message(
             except ValidationError as e:
                 logging.error("Error converting item into PubSubBookV1 object. Received: %s Error: %s", book, e)
                 continue
-            book_that_already_exist = await client.get_already_indexed_books([serialized_book.book_id])
 
-            if serialized_book.book_id in book_that_already_exist:
-                logging.info("Book %s already indexed, skipping", serialized_book.book_id)
-                continue
-            else:
-                await client.create_book(serialized_book.dict())
+            await client.create_book(serialized_book.dict())
             indexed += 1
+            successful_books.append(book)
     except BookRecommenderApiClientException as e:
         logging.error("API returned 4xx exception when called with payload %s - exception: %s", serialized_book, e)
     except BookRecommenderApiServerException as e:
         logging.error("API returned 5xx Exception when called with payload %s - exception: %s", serialized_book, e)
         return Response(status_code=HTTP_500_INTERNAL_SERVER_ERROR)
+
+    if len(successful_books) > 0:
+        pubsub_audit_client.send_batch(get_properties().pubsub_book_audit_topic_name, successful_books)
 
     # We need to return 200 to pubsub, otherwise it will retry
     return IndexerResponse(indexed=indexed)
